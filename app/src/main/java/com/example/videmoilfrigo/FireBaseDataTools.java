d75601ba1982da3df11e7705e42cde3cb03adf2f
Romain ROCHE <rocheromain@live.fr>
2019-12-10 17:50:14 -0500

    First commit

--- app/src/main/java/com/example/videmoilfrigo/FireBaseDataTools.java
+++ app/src/main/java/com/example/videmoilfrigo/FireBaseDataTools.java
@@ -4,10 +4,8 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.annotations.Nullable;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
